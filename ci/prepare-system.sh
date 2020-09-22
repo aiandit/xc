efba78c0acaa6e3f5ca62968cbeee74ead784d02
Johannes Willkomm <info@ai-and-it.de>
2020-09-22 11:20:27 +0200

    CI

--- ci/prepare-system.sh
+++ ci/prepare-system.sh
@@ -2,5 +2,9 @@
 
 # https://uwsgi-docs.readthedocs.io/en/latest/tutorials/Django_and_nginx.html
 
+apt-get update
+
+apt-get install aptitude
+
 aptitude install -y python3 python3-decorator nginx uwsgi-plugin-python3 uwsgi-emperor
 python3 -m pip install Django==3.0.7
