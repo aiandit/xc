53bbbd8e0ea2d8c2d3b8f50d8c15fcb13beda9b2
Johannes Willkomm <info@ai-and-it.de>
2020-10-13 15:47:38 +0200

    CI: Missing option -y

--- ci/prepare-system.sh
+++ ci/prepare-system.sh
@@ -2,9 +2,9 @@
 
 # https://uwsgi-docs.readthedocs.io/en/latest/tutorials/Django_and_nginx.html
 
-apt-get update
+apt-get -y update
 
-apt-get install aptitude
+apt-get -y install aptitude
 
 aptitude install -y python3 python3-decorator python3-brotli nginx uwsgi-plugin-python3 uwsgi-emperor
 python3 -m pip install Django==3.1.1
