e0939b3baafa1c4d56bc8e2046ec822c2111ab07
Johannes Willkomm <johannes@johannes-willkomm.de>
2021-05-09 15:02:18 +0200

    CI

--- ci/prepare-system.sh
+++ ci/prepare-system.sh
@@ -6,5 +6,5 @@ apt-get -y update
 
 apt-get -y install aptitude
 
-aptitude install -y python3 python3-decorator python3-brotli nginx uwsgi-plugin-python3 uwsgi-emperor
+aptitude install -y python3 python3-decorator python3-brotli python3-pip nginx uwsgi-plugin-python3 uwsgi-emperor
 python3 -m pip install Django==3.1.1
