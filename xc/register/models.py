bfef52d480a41ed4a123434ecf7829f93dddb3e5
Johannes Willkomm <johannes@johannes-willkomm.de>
2021-05-22 00:11:23 +0200

    Actcode updated

--- xc/register/models.py
+++ xc/register/models.py
@@ -19,10 +19,15 @@ salt='acctcode9d898'
 class ActivationCode(models.Model):
     code = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator', null=True)
+    email = models.TextField(max_length=100, default='')
     userip = models.ForeignKey(UserIP, on_delete=models.CASCADE)
     mode = models.TextField(max_length=10, default='acc.act')
     pub_date = models.DateTimeField('date created', default=timezone.now)
     mod_date = models.DateTimeField('date changed', default=timezone.now)
+    def asxml(self):
+        return "<acode><id>%s</id><user>%s</user><creator>%s</creator><email>%s</email><date>%s</date><mode>%s</mode></acode>" % \
+            (self.code, self.user, self.creator, self.email, self.mod_date.timestamp(), self.mode)
     def __unicode__(self):
         return "%s" % self.sign()
     def sign(self):
